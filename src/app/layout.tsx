cee0f6c66bbdd5317822925992ff01ca48ce6a1f
Meron Seyoum <meryato@gmail.com>
2025-10-10 17:41:15 -0600

    env update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places,geometry&loading=async`}
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PRIVATE_GOOGLE_MAPS_API_KEY}&libraries=places,geometry&loading=async`}
           strategy="lazyOnload"
         />
       </head>
